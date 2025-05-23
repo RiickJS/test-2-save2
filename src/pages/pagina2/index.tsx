77ffbf79c2e8a68b831255ea39dcace1ef479671
Test <test@site-test.com>
2025-05-23 20:01:53 +0000

    Update site

--- src/pages/pagina2/index.tsx
+++ src/pages/pagina2/index.tsx
@@ -1,11 +1,34 @@
 import Head from "next/head";
+import { TestBlock } from "@/components/TestBlock";
+import { TestBlock2 } from "@/components/TestBlock2";
 export default function Page() {
   return (
     <>
       <Head>
         <title>Pagina2</title>
       </Head>
-      <></>
+      <>
+        <TestBlock2
+          t1="Slow-carb next level shoindxgoitch ethical authentic, scenester sriracha forage."
+          t2="Button"
+        />
+        <TestBlock
+          t1="Slow-carb next level shoindxgoitch ethical authentic, scenester sriracha forage."
+          t2="Button"
+        />
+        <TestBlock
+          t1="Slow-carb next level shoindxgoitch ethical authentic, scenester sriracha forage."
+          t2="Button"
+        />
+        <TestBlock
+          t1="Slow-carb next level shoindxgoitch ethical authentic, scenester sriracha forage."
+          t2="Button"
+        />
+        <TestBlock
+          t1="Slow-carb next level shoindxgoitch ethical authentic, scenester sriracha forage."
+          t2="Button"
+        />
+      </>
     </>
   );
 }
