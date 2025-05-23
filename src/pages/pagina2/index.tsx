ef83c75eb679c1177e03f5f3d29e8eebe5668f31
Test <test@site-test.com>
2025-05-23 20:03:45 +0000

    Update site

--- src/pages/pagina2/index.tsx
+++ src/pages/pagina2/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { TestBlock } from "@/components/TestBlock";
 import { TestBlock2 } from "@/components/TestBlock2";
+import { tailblocksblock2 } from "@/components/tailblocksblock2";
 export default function Page() {
   return (
     <>
@@ -24,6 +25,7 @@ export default function Page() {
           t1="Slow-carb next level shoindxgoitch ethical authentic, scenester sriracha forage."
           t2="Button"
         />
+        <tailblocksblock2 t1="Block2" />
         <TestBlock
           t1="Slow-carb next level shoindxgoitch ethical authentic, scenester sriracha forage."
           t2="Button"
